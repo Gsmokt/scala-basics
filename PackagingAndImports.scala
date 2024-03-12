62abe927bbe4a8873f1c9de22306d1a1095a1c3b
gitpb <biarth@wp.pl>
2024-03-12 23:16:26 +0100

    Packages

---
+++ PackagingAndImports.scala
@@ -0,0 +1,17 @@
+package part2op
+
+//import package_name.{classes_files} or
+//import package_name._ import all
+
+// /import package_name.{classes_files => another_name} rename class
+
+// can be one global object per package, then methods can be invoke
+// independently outside the object - in the same package scope !!
+
+// other staff just like in Go, package hierarchy etc. (package - folder),
+// just no need capital letter to be export
+
+
+object PackagingAndImports extends App {
+
+}
