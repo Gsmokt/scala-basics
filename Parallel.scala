ebce12c223e459c4dbc09b3b1cad7ce1adb6c433
gitpb <biarth@wp.pl>
2024-04-01 23:53:18 +0200

    Update Parallel.scala

--- Parallel.scala
+++ Parallel.scala
@@ -1,45 +1,83 @@
 package lectures.part3concurrency
 
-import java.util.concurrent.Executors
+import java.awt.PrintJob
 
-object Parallel extends App {
+object JWMConcurrencyProblems {
+  def runInParallel(): Unit =
+    var x = 0
+    val thread1 = new Thread(() => {
+      x = 1
+    })
+   
+    val thread2 = new Thread(() => {
+      x = 2
+    })
+    
+    thread1.start()
+    thread2.start()
+    println(x)
   
-  // clear parallel
+  class BankAccount(var amount: Int)
   
-  val aThread = new Thread(new Runnable {
-    override def run(): Unit = println("Running in parallel")
-  })
+  def buy(account: BankAccount,thing:String,price:Int):Unit =
+    account.amount -= price
 
-  aThread.start()
+  def buySafe(account: BankAccount,thing:String,price:Int):Unit =
 
-  val threadHello = new Thread(() => (1 to 5).foreach(_ => println("hello")))
-  val threadGoodbye = new Thread(() => (1 to 5).foreach(_ => println("goodbye")))
+    // !!!!!!!!!!!!!!!!
+    // synchronized works the same as mutex.Lock and mutex.Unlock in Go
+    // but here in parallel, but in this example result with threads
+    // is the same as channels and goroutines in Go
+    account.synchronized {
+      account.amount -= price
+    }
 
-  threadHello.start()
-  threadGoodbye.start()
-
-  val pool = Executors.newFixedThreadPool(10)
-  pool.execute(() => println("something in the thread pool"))
-
-  pool.execute(() => {
-    Thread.sleep(1000)
-    println("done after 1 sec")
-  })
-
-  pool.execute(() => {
-    Thread.sleep(1000)
-    println("almost done")
-    Thread.sleep(1000)
-    println("done after 2 sec")
-  })
-
-  // it shut the thread and all executors run after shut,  but existing execution 
-  pool.isShutdown // we can check the shut status in this way
-  // is still running
-  pool.shutdown()
-  pool.execute(() => println("should not appear"))
+  def inceptionThreads(maxThreads: Int, i: Int = 1): Thread =
+    new Thread(() => {
+      if (i < maxThreads) {
+        val newThread = inceptionThreads(maxThreads, i + 1)
+        // works the same as in Go, new thread (like goroutine) 
+        // start, join to block this piece of code like: Lock, Unlock, wg.Done() 
+        // goroutine finishes job, close channel if buffer or read/write data by channel
+        // to prevent deadlock
+        newThread.start()
+        newThread.join()
+      }
+      println(s"Hello from thread $i")
+    })
   
-  // but that close everything immediately, and running execution will 
-  // throw exception
-  pool.shutdownNow()
+  def minMax(): Unit =
+    var x = 0
+    val threads = (1 to 100).map(_ => new Thread(() => x += 1))
+    threads.foreach(_.start())
+    
+  def demoSleepFallacy(): Unit  =
+    var message = ""
+    val awesomeThread = new Thread(() => {
+      Thread.sleep(1000)
+      message = "Scala is awesome"
+    })
+    
+    message = "Scala sucks"
+    awesomeThread.start()
+    Thread.sleep(1001)
+    println(message)
+    
+  def demoBankingProblem(): Unit =
+    (1 to 10000).foreach { _ => 
+      val account = BankAccount(50000)
+      val tThread1 = new Thread(() => buy(account,"shoes",3000))
+      val tThread2 = new Thread(() => buy(account,"iPhone",4000))
+      tThread1.start()
+      tThread2.start()
+      tThread1.join()
+      tThread2.join()
+      if(account.amount != 430000) println(s"Aha i've just broken the bank: ${account.amount}")
+      
+    }
+    
+  def main(array: Array[String]): Unit = {
+    runInParallel()
+    inceptionThreads(50).start()
+  }
 }
